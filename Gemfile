ead43295e6cebcb1be61f08bf8759f4b890c8b58
lukesmith <stuff@lukesmith.net>
2011-12-16 23:30:15 +0000

    install clones and initializes a new git repository

--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "http://rubygems.org"
 
 # Specify your gem's dependencies in gitplate.gemspec
 gemspec
+
+gem "git", "~> 1.2.5"
--- lib/gitplate/gitplate.rb
+++ lib/gitplate/gitplate.rb
@@ -1,3 +1,5 @@
+require 'Git'
+
 module Gitplate
 
   VERSION = "0.0.1"
@@ -9,8 +11,36 @@ module Gitplate
     system args.join ' '
   end
 
-  def self.install
-    puts "fasfas"
+  def self.install(name, repository)
+    if (File.directory? name)
+      raise "Directory already exists"
+    end
+
+    puts "creating #{name} based on #{repository}"
+
+    out_path = File.expand_path(File.join(name, 'tmp', 'checkout'))
+    FileUtils.mkdir_p out_path
+    
+    Git.clone(repository, name, :path => out_path)
+
+    # move the repository files to the main directory
+    files = Dir.glob("#{out_path}/#{name}/*") - [name]
+    FileUtils.mkdir name unless File.directory? name
+    FileUtils.cp_r files, name
+
+    # get rid of the temporary checkout directory
+    clear_directory File.expand_path(File.join(name, 'tmp'))
+
+    Dir.chdir name do
+      Git.init
+    end
+  end
+
+  def self.clear_directory(dir)
+    if (File.directory?(dir))
+      full_path = File.expand_path(dir)
+      FileUtils.rm_rf full_path
+    end
   end
   
 end
