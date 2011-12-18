ded386f7a964fb78b6af48af0fd37a34ee6dc02b
lukesmith <stuff@lukesmith.net>
2011-12-18 14:21:59 +0000

    Changed format of the generated config.yml

--- lib/gitplate/gitplate.rb
+++ lib/gitplate/gitplate.rb
@@ -18,7 +18,8 @@ module Gitplate
     out_path = File.expand_path(File.join(name, 'tmp', 'checkout'))
     FileUtils.mkdir_p out_path
     
-    Git.clone(repository, name, :path => out_path)
+    source_repository = Git.clone(repository, name, :path => out_path)
+    source_repository_sha = source_repository.object('HEAD').sha
 
     # move the repository files to the main directory
     files = Dir.glob("#{out_path}/#{name}/*") - [name]
@@ -30,7 +31,11 @@ module Gitplate
       clear_directory File.expand_path(File.join('tmp'))
 
       create_gitplate_file
-      update_config_with :project_name, name
+      update_config_with({
+          :project => { :name => name },
+          :repository => { :url => repository, :sha => source_repository_sha },
+          :gitplate_version => Gitplate::VERSION
+        })
 
       # pull in the plate file from the cloned repository
       plate_file = 'plate'
@@ -58,7 +63,7 @@ module Gitplate
         'plate',
         task,
         {
-          :project_name => config["project_name"],
+          :project_name => config["project"]["name"],
           :project_dir => Dir.pwd
         })
   end
@@ -75,9 +80,13 @@ module Gitplate
     YAML.load(File.open(config_file))
   end
 
-  def self.update_config_with(key, value)
+  def self.update_config_with(values)
     config = load_gitplate_file
-    config[key.to_s] = value
+
+    formatted = format_hash_for_yaml values
+    formatted.each do |name, value|
+      config[name.to_s] = value
+    end
 
     File.open(config_file, 'w') { |f| YAML.dump(config, f) }
   end
@@ -104,6 +113,20 @@ module Gitplate
       full_path = File.expand_path(dir)
       FileUtils.rm_rf full_path
     end
+  end
+
+  def self.format_hash_for_yaml(hash)
+    result = Hash.new
+
+    hash.each do |name, value|
+      if (value.kind_of? Hash)
+        result[name.to_s] = format_hash_for_yaml value
+      else
+        result[name.to_s] = value
+      end
+    end
+
+    result
   end
   
 end
